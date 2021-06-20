03cbb2ca8d8320986bfe1772ea7736c5fab9fb5d
Alex <mendelberg44@gmail.com>
2021-06-20 11:28:17 +0300

    l2 t1

--- l2/t1/src/index.js
+++ l2/t1/src/index.js
@@ -24,7 +24,7 @@ const counterReducer = (state = 0, action) => {
   }
 };
 
-// const store = createStore(counterReducer);
+const store = createStore(counterReducer);
 // store.dispatch(increment());
 // store.dispatch(increment());
 // store.dispatch(increment());
