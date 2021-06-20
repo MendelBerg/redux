48ce5fc08cfb8f8cfba1b8e54c6d2e2213013533
Alex <mendelberg44@gmail.com>
2021-06-20 10:20:11 +0300

    l1 t1 v4

--- l1/t1/src/ThemedButton.jsx
+++ l1/t1/src/ThemedButton.jsx
@@ -4,17 +4,14 @@ import { ThemeContext } from './themes-context.js';
 class ThemedButton extends React.Component {
   render() {
     return (
-      <div>
-        {' '}
-        <button
-          style={{
-            background: this.context.background,
-            color: this.context.fontColor,
-          }}
-          {...this.props}
-          className="btn"
-        />
-      </div>
+      <button
+        {...this.props}
+        style={{
+          backgroundColor: this.context.background,
+          color: this.context.fontColor,
+        }}
+        className="btn"
+      />
     );
   }
 }
