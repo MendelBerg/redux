a4a77c1a017ed3c348e1bc739f39f644e16401e9
Alex <mendelberg44@gmail.com>
2021-06-20 13:41:33 +0300

    l3 t1

---
+++ l3/h1/src/index.js
@@ -0,0 +1,32 @@
+import { createStore } from 'redux';
+
+export const INCREMENT = 'COUNTER/INCREMENT';
+export const DECREMENT = 'COUNTER/DECREMENT';
+
+export const increment = () => {
+  return { type: INCREMENT };
+};
+
+export const decrement = () => {
+  return { type: DECREMENT };
+};
+
+export const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+
+    case DECREMENT:
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+export const store = createStore(counterReducer);
+// store.dispatch(increment());
+// store.dispatch(increment());
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// console.log(store.getState());
---
+++ l3/t1/src/index.js
@@ -0,0 +1,15 @@
+import store from './store.js';
+import { increment, decrement, reset } from './counter.actions.js';
+import { addUser, deleteUser, updateUser } from './users.actions.js';
+
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(reset());
+
+store.dispatch(addUser({ id: 1, name: 'Tom' }));
+store.dispatch(addUser({ id: 2, name: 'Bob' }));
+store.dispatch(deleteUser(1));
+store.dispatch(updateUser(2, { name: 'Bob Marlin', age: 17 }));
