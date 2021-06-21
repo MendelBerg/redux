f62c6c2f1c3abe0ebd894924be6cbda225762c11
Alex <mendelberg44@gmail.com>
2021-06-21 20:23:48 +0300

    l6 t1

--- l1/h1/webpack.config.js
+++ l1/h1/webpack.config.js
@@ -2,14 +2,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
   const config = {
     entry: './src/index.jsx',
     output: {
-      filename: 'bundle.js',
-      publicPath: '/',
+      // filename: 'bundle.js',
+      // publicPath: '/',
+      path: path.resolve('./', 'review_build'),
     },
     module: {
       rules: [
